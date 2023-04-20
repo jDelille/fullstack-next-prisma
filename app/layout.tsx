337aa6a9627c14f275b58196795e2c008b60be84
Justin Delille <justind7513@gmail.com>
2023-04-20 14:36:54 -0700

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {/* NAVBAR */}
+        {children}
+      </body>
     </html>
   )
 }
