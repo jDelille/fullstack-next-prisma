8d82f65cf9a499448d6b14432a6bb6958f50b3f5
Justin Delille <justind7513@gmail.com>
2023-05-26 15:53:55 -0700

    fix: added user param to post feed

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -2,10 +2,12 @@ import dynamic from 'next/dynamic';
 import getPosts from '../actions/getPosts';
 import getCurrentUser from '../actions/getCurrentUser';
 import styles from './page.module.scss';
+import getUsers from '../actions/getUsers';
 
 const Explore = async () => {
  const posts = await getPosts();
  const currentUser = await getCurrentUser();
+ const users = await getUsers();
 
  const DynamicPostFeed = dynamic(
   () => import('../components/post-feed/PostFeed'),
@@ -19,7 +21,7 @@ const Explore = async () => {
    <div className={styles.exploreHeader}>
     Explore
    </div>
-   <DynamicPostFeed posts={posts} currentUser={currentUser} />
+   <DynamicPostFeed posts={posts} currentUser={currentUser} users={users} />
   </div>
  );
 }
