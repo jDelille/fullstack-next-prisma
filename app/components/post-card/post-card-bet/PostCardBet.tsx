6b85784b66919fe09a2688bf0628015fcf015f0a
Justin Delille <justind7513@gmail.com>
2023-05-27 21:38:21 -0700

    fix: updated badge colors and bet design

--- app/components/post-card/post-card-bet/PostCardBet.tsx
+++ app/components/post-card/post-card-bet/PostCardBet.tsx
@@ -6,7 +6,6 @@ type PostCardBetProps = {
 };
 
 const PostCardBet: React.FC<PostCardBetProps> = ({ post: bet }) => {
-
 	const favOrDogBadge = () => {
 		if (bet.favorite) {
 			return <div className={styles.favBadge}>Fav</div>;
@@ -17,38 +16,55 @@ const PostCardBet: React.FC<PostCardBetProps> = ({ post: bet }) => {
 
 	const winOrLossBadge = () => {
 		if (bet.status === 'win') {
-			return <div className={styles.winBadge}>Win</div>
-		} else if (bet.status === "loss") {
-			return <div className={styles.lossBadge}>Loss</div>
+			return <div className={styles.winBadge}>Win</div>;
+		} else if (bet.status === 'loss') {
+			return <div className={styles.lossBadge}>Loss</div>;
 		} else if (bet.status === 'open') {
-			return <div className={styles.openBadge}>Open</div>
-
+			return <div className={styles.openBadge}>Open</div>;
 		}
-	}
+	};
 
 	return (
 		<div className={styles.postBet}>
-
 			<div className={styles.postBetStatus}>
 				{winOrLossBadge()}
-				<p className={styles.betOdds}>{bet.odds}</p>
+			</div>
 
-			</div>
-			<div className={styles.postBetHeader}>
+			{/* <div className={styles.postBetHeader}>
 				<p>{bet.name}</p>
-			</div>
+			</div> */}
+
 			<div className={styles.postBetBody}>
 				{bet.location === 'away' ? (
-					<div className={styles.team}>{bet.awayTeam} {favOrDogBadge()}</div>
+					<div className={styles.team}>
+						{bet.awayTeam}
+						{favOrDogBadge()}
+						<p className={styles.betOdds}>{bet.odds}</p>
+					</div>
 				) : (
-					<div className={styles.team}>{bet.homeTeam} {favOrDogBadge()}</div>
+					<div className={styles.team}>
+						{bet.homeTeam} {favOrDogBadge()}
+						<p className={styles.betOdds}>{bet.odds}</p>
+					</div>
+
 				)}
-				<p>
+
+				<p className={styles.betName}>{bet.name}</p>
+
+				<div className={styles.betType}>
 					{bet.type} <span>{bet.value}</span>
-				</p>
-				<p className={styles.wager}>Wager ${bet.wager}</p>
-				<p className={styles.payout}>Payout ${bet.payout}</p>
+				</div>
+
+				<div className={styles.wagerAndPayout}>
+					<div className={styles.wager}>
+						<p>Amount Wagered: <span>${bet.wager}</span></p>
+					</div>
+					<div className={styles.payout}>
+						Net Profit: ${bet.payout}
+					</div>
+				</div>
 			</div>
+
 			<div className={styles.disclaimer}>
 				<p>Odds shown are at time of post and are subject to change.</p>
 			</div>
