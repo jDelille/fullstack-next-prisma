c6880a3bdf275c42b62406c9080329f13425ac4c
Justin Delille <justind7513@gmail.com>
2023-05-30 15:48:13 -0700

    fix: added promise.all to profile page

--- app/groups/page.tsx
+++ app/groups/page.tsx
@@ -4,18 +4,12 @@ import getCurrentUser from '../actions/getCurrentUser';
 import NoGroupMessage from './NoGroupMessage';
 import dynamic from 'next/dynamic';
 import FeedHeader from '../components/feed-header/FeedHeader';
-import { IoArrowBack } from 'react-icons/io5';
 import { FaUsers } from 'react-icons/fa';
-import Button from '../components/button/Button';
 import CreateGroupButton from './CreateGroupButton';
 
 
-interface IParams {
- groupId?: string;
-}
 
-
-const Groups = async ({ params }: { params: IParams }) => {
+const Groups = async () => {
 
  const [groups, currentUser] = await Promise.all([getGroups(), getCurrentUser()])
 
--- app/user/[userId]/page.tsx
+++ app/user/[userId]/page.tsx
@@ -5,27 +5,25 @@ import getFollowersCount from '@/app/actions/getFollowersCount';
 import getCurrentUser from '@/app/actions/getCurrentUser';
 import getGroupsByUserId from '@/app/actions/getGroupsByUserId';
 import dynamic from 'next/dynamic';
-import { BiHash } from 'react-icons/bi';
-import { AiOutlineArrowLeft } from 'react-icons/ai';
 import FeedHeader from '@/app/components/feed-header/FeedHeader';
-import { IoIosArrowRoundBack } from 'react-icons/io';
 import { IoArrowBack } from 'react-icons/io5';
 import getUsers from '@/app/actions/getUsers';
-import { User } from '@prisma/client';
 import { SafeUser } from '@/app/types';
 import PostCardSkeleton from '@/app/components/skeletons/post-card-skeleton/PostCardSkeleton';
-
 interface IParams {
  userId?: string;
 }
 
 const ProfilePage = async ({ params }: { params: IParams }) => {
- const user = await getUserById(params);
- const posts = await getPostsByUserId(params);
- const followerCount = await getFollowersCount(params);
- const currentUser = await getCurrentUser();
- const groups = await getGroupsByUserId(params);
- const users = await getUsers()
+
+ const [user, posts, followerCount, currentUser, groups, users] = await Promise.all([
+  getUserById(params),
+  getPostsByUserId(params),
+  getFollowersCount(params),
+  getCurrentUser(),
+  getGroupsByUserId(params),
+  getUsers(),
+ ]);
 
  const DynamicPostFeed = dynamic(
   () => import('../../components/post-feed/PostFeed'),
